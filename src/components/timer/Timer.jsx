a3afd8cbed24b5f10a1a754c466ce097980d7c1f
Bhaskar Das <bhaskardas090@gmail.com>
2022-01-24 21:50:08 +0530

    Timer added and working perfectly

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './timer.module.css';
 
 export default function Timer() {
+  const [time, setTime] = useState(30);
+
+  useEffect(() => {
+      const timer = time > 0 && setTimeout(()=>{
+        setTime(prevTime => prevTime - 1);
+      },1000);
+      return () => clearTimeout(timer);
+  }, [time]);
+  
+  
   return (
     <div className={styles.timer}>
-      <button className={styles.timerButton}>30</button>
+      <button className={styles.timerButton}>{time}</button>
     </div>
   );
 }
